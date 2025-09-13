cac83a58f0b66e1ccf6167ed61924f8329ab3cfc
Hijran9999 <119710869+Hijran9999@users.noreply.github.com>
2025-09-13 11:11:57 -0400

    Refactor App component to IngredientsList and pass items as props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function IngredientsList(props) {
+  return React.createElement(
+    "ul",
+    { className: "ingredients" },
+    props.items.map((ingredient, i) =>
+      React.createElement("li", { key: i }, ingredient)
+    )
   );
 }
 
-export default App;
+export default IngredientsList;
